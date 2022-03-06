c33790fbc736534a124511792d8ccf1901504de1
Gigalo <3163083993@qq.com>
2022-03-06 22:04:11 +0800

    fxxk

---
+++ src/edu/hdu/G1g4locat/exception/WebConfigDuplicatedException.java
@@ -0,0 +1,7 @@
+package edu.hdu.G1g4locat.exception;
+
+public class WebConfigDuplicatedException extends Exception {
+    public WebConfigDuplicatedException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/edu/hdu/G1g4locat/utils/ContextXmlUtil.java
@@ -0,0 +1,21 @@
+package edu.hdu.G1g4locat.utils;
+
+import cn.hutool.core.io.FileUtil;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+
+public class ContextXmlUtil {
+    public String getWatchedResource() {
+        try {
+            String xml = FileUtil.readUtf8String(Constant.contextXmlFile);
+            Document d = Jsoup.parse(xml);
+            Element e = d.selectFirst("WatchedResource");
+            return e.text();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "WEB-INF/web.xml";
+        }
+    }
+}
